37fe3f9a3bb543c3e816e648bfc7ccb31e2fb91c
Aaron Slots <85274163+AaronSlots@users.noreply.github.com>
2018-11-07 16:03:13 +0100

    Forms added

---
+++ Festispec.Database/Migrations/20181107145923_Forms.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Festispec.Database.Migrations
+{
+    public partial class Forms : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Forms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    IsTemplate = table.Column<bool>(nullable: false),
+                    Comments = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Forms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Question",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Text = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    QuestionType = table.Column<int>(nullable: false),
+                    FormId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Question", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Question_Forms_FormId",
+                        column: x => x.FormId,
+                        principalTable: "Forms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QuestionItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Body = table.Column<string>(nullable: true),
+                    QuestionId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuestionItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QuestionItem_Question_QuestionId",
+                        column: x => x.QuestionId,
+                        principalTable: "Question",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Question_FormId",
+                table: "Question",
+                column: "FormId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionItem_QuestionId",
+                table: "QuestionItem",
+                column: "QuestionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QuestionItem");
+
+            migrationBuilder.DropTable(
+                name: "Question");
+
+            migrationBuilder.DropTable(
+                name: "Forms");
+        }
+    }
+}
--- Festispec.Database/src/Program.cs
+++ Festispec.Database/src/Program.cs
@@ -1,18 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Festispec.Domain;
-using Microsoft.EntityFrameworkCore;
 
-namespace Festispec.Database
+namespace Festispec.Database.src
 {
-    class FestispecContext : DbContext
-    {
-        public DbSet<Inspection> Inspections { get; set; }
-        public DbSet<Employee> Employees { get; set; }
-        public DbSet<Role> Roles { get; set; }
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Application> Applications { get; set; }
+	class Program
+	{
+		public static void Main(string[] args)
+		{
 
-    }
+		}
+	}
 }
